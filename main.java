80c93661c90befc24af170fecf53f2d39411516f
Velin Andreev <63392930+Velin-A@users.noreply.github.com>
2020-04-20 16:31:13 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        //calculatingRectangle();
-        //daysinthemonths();
-        //crazyCheeter();
+        calculatingRectangle();
+        daysinthemonths();
+        crazyCheeter();
     }
 
     public static void calculatingRectangle() {
